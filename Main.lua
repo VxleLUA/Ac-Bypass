aef9a4e4ae41dd94fe7dba1b942df7fe6c7a9fb8
VxleLUA <128989005+VxleLUA@users.noreply.github.com>
2023-05-25 14:43:21 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,38 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
-    wait(0.3)
-        loadstring(game:HttpGet("raw.githubusercontent.com/VxleLUA/Ac-Bypass/main/Ok.lua", true))()
+local function deleteExcessiveBoxHandleAdornments(parent)
+    local boxHandleAdornmentCount = 0
+    local boxHandleAdornments = {}
+
+    local function collectBoxHandleAdornments(guiObject)
+        for _, child in ipairs(guiObject:GetDescendants()) do
+            if child:IsA("BoxHandleAdornment") then
+                boxHandleAdornmentCount = boxHandleAdornmentCount + 1
+                table.insert(boxHandleAdornments, child)
+            end
+        end
+    end
+
+    collectBoxHandleAdornments(parent)
+
+    if boxHandleAdornmentCount > 3 then
+        for _, adornment in ipairs(boxHandleAdornments) do
+            adornment:Destroy()
+        end
+    end
+end
+
+-- Load and run the first script
+local firstScript = coroutine.create(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+end)
+coroutine.resume(firstScript)
+
+-- Delay for 2 seconds
+wait(2)
+
+-- Load and run the second script
+local secondScript = coroutine.create(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VxleLUA/Ac-Bypass/main/Ok.lua", true))()
+end)
+coroutine.resume(secondScript)
+
+-- Perform actions after both scripts have executed (if needed)
